af04ba1311ff00bed78082022c890da6506bdfd2
Yan <93109473+polyansofine@users.noreply.github.com>
2022-08-26 04:46:36 +0300

    enum

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,58 @@
+#[derive(Debug,PartialEq)]
+enum FileState {
+    Open,
+    Closed,
+}
+
 #[derive(Debug)]
-enum Event {
-    Update,
-    Delete,
-    Unknown,
+struct File {
+    name: String,
+    data:Vec<u8>,
+    state:FileState,
 }
 
-type Message = String;
-
-fn parse_log(line:&str) -> (Event,Message) {
-    let parts:Vec<_> = line.splitn(2, ' ').collect();
-    if parts.len() == 1 {
-        return (Event::Unknown,String::from(line));
+impl File {
+    fn new(name:&str) -> File {
+        File{
+            name:String::from(name),
+            data:Vec::new(),
+            state:FileState::Closed
+        }
     }
-    let event = parts[0];
-    let rest = String::from(parts[1]);
-    match event {
-        "UPDATE" | "update" => (Event::Update,rest),
-        "DELETE" | "delete" => (Event::Delete,rest),
-        _ => (Event::Unknown,String::from(line)),
+    fn read(self:&File,save_to:&mut Vec<u8>) -> Result<usize,String> {
+        if self.state != FileState::Open {
+            return Err(String::from("File must be open for reading"));
+        }
+        let mut temp = self.data.clone();
+        let read_length = temp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut temp);
+        Ok(read_length)
     }
 }
+
+fn open(mut f:  File) -> Result<File,String> {
+    f.state = FileState::Open;
+    Ok(f)
+}
+fn close(mut f:  File) -> Result<File,String> {
+    f.state = FileState::Closed;
+    Ok(f)
+}
+
 
 fn main() {
-    let log = "BEGIN Transaction XK342 
-UPDATE 234:LS/32231 {\"price\": 31.00} -> {\"price\": 40.00} 
-DELETE 342:LO/22111";
-    for line in log.lines() {
-        let parse_result = parse_log(line);
-        println!("{:?}",parse_result);
+    let mut f5 = File::new("5.txt");
+    let mut buffer:Vec<u8> = vec![];
+    f5= open(f5).unwrap();
+    if f5.read(&mut buffer).is_err() {
+        println!("Error checking is working");
     }
+    let f5_length = f5.read(&mut buffer).unwrap();
+    f5 = close(f5).unwrap();
+    let text = String::from_utf8_lossy(&buffer);
 
+    println!("{:?}",f5);
+    println!("{} is {} bytes long",&f5.name,f5_length);
+    println!("{}",text);
 }
