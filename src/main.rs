b768a13c6f5230ad7d613d70c142da91bbccc626
Yan <93109473+polyansofine@users.noreply.github.com>
2022-08-26 07:47:57 +0300

    display

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,50 @@
+#![allow(dead_code)]              // <1>
+
+use std::fmt;                     // <2>
+use std::fmt::{Display};          // <3>
+
 #[derive(Debug,PartialEq)]
 enum FileState {
-    Open,
-    Closed,
+  Open,
+  Closed,
 }
 
 #[derive(Debug)]
 struct File {
-    name: String,
-    data:Vec<u8>,
-    state:FileState,
+  name: String,
+  data: Vec<u8>,
+  state: FileState,
+}
+
+impl Display for FileState {
+   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+     match *self {
+         FileState::Open => write!(f, "OPEN"),      // <4>
+         FileState::Closed => write!(f, "CLOSED"),  // <4>
+     }
+   }
+}
+
+impl Display for File {
+   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      write!(f, "<{} ({})>",
+	         self.name, self.state)              // <5>
+   }
 }
 
 impl File {
-    fn new(name:&str) -> File {
-        File{
-            name:String::from(name),
-            data:Vec::new(),
-            state:FileState::Closed
-        }
+  fn new(name: &str) -> File {
+    File {
+        name: String::from(name),
+        data: Vec::new(),
+        state: FileState::Open,
     }
-    fn read(self:&File,save_to:&mut Vec<u8>) -> Result<usize,String> {
-        if self.state != FileState::Open {
-            return Err(String::from("File must be open for reading"));
-        }
-        let mut temp = self.data.clone();
-        let read_length = temp.len();
-        save_to.reserve(read_length);
-        save_to.append(&mut temp);
-        Ok(read_length)
-    }
+  }
 }
-
-fn open(mut f:  File) -> Result<File,String> {
-    f.state = FileState::Open;
-    Ok(f)
-}
-fn close(mut f:  File) -> Result<File,String> {
-    f.state = FileState::Closed;
-    Ok(f)
-}
-
 
 fn main() {
-    let mut f5 = File::new("5.txt");
-    let mut buffer:Vec<u8> = vec![];
-    f5= open(f5).unwrap();
-    if f5.read(&mut buffer).is_err() {
-        println!("Error checking is working");
-    }
-    let f5_length = f5.read(&mut buffer).unwrap();
-    f5 = close(f5).unwrap();
-    let text = String::from_utf8_lossy(&buffer);
-
-    println!("{:?}",f5);
-    println!("{} is {} bytes long",&f5.name,f5_length);
-    println!("{}",text);
+  let f6 = File::new("f6.txt");
+  //...
+  println!("{:?}", f6);           // <6>
+  println!("{}", f6);             // <7>
 }
