4a97ad1239d8e3d03b1ecdfa0c8299fd2d0cd864
Yan <93109473+polyansofine@users.noreply.github.com>
2022-09-07 06:22:17 +0300

    info

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,21 @@
-#![allow(unused_variables)]
-#[derive(Debug)]
+#[derive(Debug,Clone,Copy)]
 struct CubeSat {
     id:u64
 }
 
-#[derive(Debug)]
-struct Mailbox {
-    message:Vec<Message>
+#[derive(Debug,Clone,Copy)]
+enum StatusMessage {
+    Ok,
 }
 
-#[derive(Debug)]
-struct Message {
-    to:u64,
-    content:String,
+fn check_status(sat_id:CubeSat) -> StatusMessage {
+    StatusMessage::Ok
 }
-
-struct GroundStation {}
-
-impl Mailbox {
-    fn post(&mut self,msg:Message) {
-        self.message.push(msg);
-    }
-    fn deliver(&mut self,recipient:&CubeSat) -> Option<Message> {
-        for i in 0..self.message.len() {
-            if self.message[i].to == recipient.id {
-                let msg = self.message.remove(i);
-                return Some(msg);
-            }
-        }
-        None
-    }
-}
-
-impl GroundStation {
-    fn connect(&self,sat_id:u64) -> CubeSat {
-        CubeSat { id: sat_id, }
-    }
-    fn send(&self,mailbox:&mut Mailbox,msg:Message) {
-        mailbox.post(msg);
-    }
-}
-
-impl CubeSat {
-    fn recv(&self,mailbox:&mut Mailbox,) -> Option<Message> {
-        mailbox.deliver(&self)
-    }
-}
-
-fn fetch_sat_ids() -> Vec<u64> {
-    vec![1,2,3]
-}
-
-
 
 fn main() {
-    let mut mail = Mailbox {message:vec![]};
-    let base = GroundStation {};
-    let sat_ids = fetch_sat_ids();
-    for sat_id in sat_ids {
-        let sat = base.connect(sat_id);
-        let msg = Message{to:sat_id,content:String::from("hello")};
-        base.send(&mut mail, msg);
-    }
-    let sat_ids = fetch_sat_ids();
-
-    for sat_id in sat_ids {
-        let sat = base.connect(sat_id);
-        let msg = sat.recv(&mut mail);
-        println!("{:?}:{:?}",sat,msg);
-    }
+    let sat_a = CubeSat{id:0};
+    let a_status = check_status(sat_a.clone());
+    println!("a:{:?}",a_status.clone());
+    let a_status = check_status(sat_a);
+    println!("a:{:?}",a_status);
 }
